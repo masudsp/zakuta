a1b70d468df56401e6f260d855e276af2299300f
masood <jango@localhost.localdomain>
2015-11-19 19:18:35 -0800

    Adding initial install files and directory structure for Zakuta project. This is a begining of a new way of looking at content matching.

---
+++ src/zakuta_main.c
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2015, Masood Mehmood <sp@linux.com>
+ * 3-clause (or "modified") BSD license
+ *
+ * \file   zakuta_main.c
+ * \brief  main entry file
+ *
+ *
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <assert.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <signal.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <pcap.h>
+
+int
+main(int argc, char **argv)
+{
+
+
+    return 0;
+}
+
